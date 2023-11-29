cbaa6761add4afe1eed7c6acddee491dacd891e5
karkiayush <karkiayush619@gmail.com>
2023-11-29 22:12:15 +0545

    Half work of integrating api in app is done

--- app/src/main/java/com/example/weathersphere/ApiInterface.kt
+++ app/src/main/java/com/example/weathersphere/ApiInterface.kt
@@ -1,5 +1,6 @@
 package com.example.weathersphere
 
+import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -10,5 +11,5 @@ interface ApiInterface {
         @Query("q") city:String,
         @Query("appid") appid:String,
         @Query("units") units:String,
-    )
+    ):Call<WeatherApp>
 }
--- app/src/main/java/com/example/weathersphere/MainActivity.kt
+++ app/src/main/java/com/example/weathersphere/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.weathersphere
 
-import android.graphics.Typeface
 import android.os.Bundle
+import android.widget.SearchView
 import androidx.appcompat.app.AppCompatActivity
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.create
 
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +17,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        getWeatherData()
+    }
+
+    private fun getWeatherData() {
+        val inputFieldRef = findViewById<SearchView>(R.id.searchView)
+        val inputCity = inputFieldRef.
+        val retrofit = Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl("https://api.openweathermap.org/data/2.5/weather?")
+            .build()
+            .create(ApiInterface::class.java)
+
+        val response =
+            retrofit.getWeatherData("Kathmandu", "c9454baee04c4abfa42bf5331bd37b17", "metric")
+        response.enqueue(object : Callback<WeatherApp>{
+            override fun onResponse(call: Call<WeatherApp>, response: Response<WeatherApp>) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onFailure(call: Call<WeatherApp>, t: Throwable) {
+                TODO("Not yet implemented")
+            }
+
+        })
     }
 }
